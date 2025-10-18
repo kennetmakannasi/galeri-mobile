75b84d5f4d980b252a7ebd53a9afc5e68aa0bd8e
Kenneth Heagent Sulistio <kennethdiscord010@gmail.com>
2025-10-18 15:48:34 +0700

    feat: refresh screen

--- src/components/layout/mainLayout.jsx
+++ src/components/layout/mainLayout.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import Notification from "../../notificationEcho";
 import { Toaster } from "react-hot-toast";
-
+import ReactPullToRefresh from "react-pull-to-refresh";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export const SessionData = createContext()
 
@@ -15,7 +16,7 @@ export default function MainLayout() {
   const navigate = useNavigate();
   const [selfData , setSelfData] = useState(); 
   const location = useLocation()
-  
+  const [isRefresh, setIsRefresh] = useState(false);
 
   async function fetchSelfData() {
     const res = await axios.get(`${baseUrl}/api/auth/self`,{
@@ -47,15 +48,28 @@ export default function MainLayout() {
     middleware()
   },[token])
 
+  async function handleRefresh() {
+    setIsRefresh(true)
+    setTimeout(() => {
+      navigate(0)
+    }, 2000);
+  }
+
   return (
     <SessionData.Provider value={selfData}>
-      <Toaster/>
-      
+      <ReactPullToRefresh onRefresh={handleRefresh}>
+        <Toaster/>
         <Notification/>
         <div className="flex-1 mb-20">
+          {isRefresh && (
+            <div className="w-full flex items-center justify-center py-3">
+              <Icon height={40} className="text-light-gray animate-spin" icon={'mingcute:loading-fill'}/>
+            </div>
+          )}
           <Outlet />
         </div>
-        <Navbar/>
+        <Navbar/>  
+      </ReactPullToRefresh>
     </SessionData.Provider>
   );
 }
